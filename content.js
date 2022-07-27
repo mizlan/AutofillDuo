90349f808d526c186d4618bf252b45555cdbcbb9
Michael Lan <michaellan202@gmail.com>
2022-07-27 19:57:50 -0400

    all da files

---
+++ content.js
@@ -0,0 +1,57 @@
+// https://stackoverflow.com/a/61511955
+function waitForElm(selector) {
+  return new Promise(resolve => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver(mutations => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
+  });
+}
+
+// wait until "Verify" button exists; when this happens,
+// #passcode-input <input> field should already exist already
+waitForElm('.verify-button').then((b) => {
+  // input element for code
+  let inputElem = document.querySelector('#passcode-input')
+
+  // procure HOTP code
+  chrome.storage.local.get(['secret', 'count'], (res) => {
+    if (!res.secret) {
+      return;
+    }
+    console.log('count is', res.count);
+
+    var hotp = new jsOTP.hotp();
+    var code = hotp.getOtp(res.secret, res.count);
+    chrome.storage.local.set({ 'count': res.count + 1 }, () => {});
+    chrome.storage.local.get(['secret', 'count'], (res) => {
+      console.log('flood')
+      console.log(res)
+    })
+
+    // insert the HOTP code
+    inputElem.value = code;
+
+    // important - you can look in the DOM for the [event] tag next to certain
+    // HTML tags, expand that and see what events should be triggered. in this
+    // case, for example, the event is needed to un-disable the "Verify" button
+    // which begins greyed out
+    inputElem.dispatchEvent(new Event('input', { bubbles: true }));
+
+    // click "Verify" (sign in!)
+    // b.click()
+  });
+
+  // click "Verify"
+})
---
+++ popup/activation.js
@@ -0,0 +1,65 @@
+console.log('am i exist')
+window.onload = () => {
+  const button = document.querySelector('button');
+  const input = document.querySelector('input');
+  input.setAttribute('size',input.getAttribute('placeholder').length);
+  const msgArea = document.querySelector('p');
+
+  const setMsg = (msgType, msg) => {
+    msgArea.classList.remove('msg-area-info', 'msg-area-error', 'msg-area-success');
+    msgArea.classList.add({
+      'info': 'msg-area-info',
+      'error': 'msg-area-error',
+      'success': 'msg-area-success',
+    }[msgType]);
+    msgArea.innerText = msg;
+  };
+
+  button.addEventListener('click', (e) => {
+    console.log('gott it')
+    const link = input.value
+    const matchesFormat = link.match(new RegExp("^https://m-(?<hostcode>\\w+).duosecurity.com/activate/(?<key>\\w+)$"));
+    if (matchesFormat === null) {
+      setMsg('error', "wrong format... check the format again :]")
+      return;
+    }
+    const hostcode = matchesFormat.groups.hostcode;
+    const key = matchesFormat.groups.key;
+
+    const postUrl = `https://api-${hostcode}.duosecurity.com/push/v2/activation/${key}?customer_protocol=1`
+    console.log('posting to', postUrl)
+
+    setMsg('info', "working on it... (don't click away)")
+    fetch(postUrl, {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        'jailbroken': 'false',
+        'architecture': 'arm64',
+        'region': 'US',
+        'app_id': 'com.duosecurity.duomobile',
+        'full_disk_encryption': 'true',
+        'passcode_status': 'true',
+        'platform': 'Android',
+        'app_version': '3.49.0',
+        'app_build_number': '323001',
+        'version': '11',
+        'manufacturer': 'unknown',
+        'language': 'en',
+        'model': 'Pixel 3a',
+        'security_patch_level': '2021-02-01'
+      })
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setMsg('success', "the deed is done, finish activation on Duo")
+        console.log(data);
+        const hotpSecret = data['response']['hotp_secret'];
+        chrome.storage.local.set({ secret: hotpSecret, count: 0 }, () => {});
+      }).catch((e) => {
+        setMsg('error', e)
+      });
+  });
+}
