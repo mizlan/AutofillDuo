3f735bf14f59aab3dbc1bb4d3e418758f4dc2fdf
Michael Lan <michaellan202@gmail.com>
2022-07-28 12:18:04 -0400

    format: add more comments to content.js

--- content.js
+++ content.js
@@ -1,3 +1,6 @@
+// a function that generates a Promise given a CSS selector
+// that fulfills when the selector points to an existing element
+//
 // https://stackoverflow.com/a/61511955
 function waitForElm(selector) {
   return new Promise(resolve => {
@@ -22,20 +25,24 @@ function waitForElm(selector) {
 // wait until "Verify" button exists; when this happens,
 // #passcode-input <input> field should already exist already
 Promise.any([
-  // account for the (seemingly) two different Duo UIs
+  // account for the (seemingly) two different Duo UIs,
+  // for more information, see:
+  // https://help.duo.com/s/article/7118?language=en_US
   waitForElm('.verify-button'),
   waitForElm('#password')
-]).then((b) => {
-  // input element for code
+]).then((button) => {
+  // input element for code, for both Traditional/Universal
   const inputElem = document.querySelector('.passcode-input');
 
   // procure HOTP code
   chrome.storage.local.get(['secret', 'count'], (res) => {
+
     if (!res.secret) {
-      // account not set up yet
+      // account not set up yet, so don't autofill anything
       return;
     }
 
+    // generate code and update COUNT for future generations
     const hotp = new jsOTP.hotp();
     const code = hotp.getOtp(res.secret, res.count);
     chrome.storage.local.set({ 'count': res.count + 1 }, () => {});
@@ -50,8 +57,6 @@ Promise.any([
     inputElem.dispatchEvent(new Event('input', { bubbles: true }));
 
     // click "Verify" (sign in!)
-    b.click();
+    button.click();
   });
-
-  // click "Verify"
 })
